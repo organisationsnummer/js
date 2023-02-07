8b073ec5225af9645acee95b3088b7f825f71f3c
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2023-02-07 18:47:18 +0100

    Bump deps and fix local tests

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 const lib = require(process.env.FILE);
-const Organisationsnummer = process.env.FILE?.includes('esm')
-  ? lib.default
-  : lib;
+const Organisationsnummer = process.env.FILE?.includes('cjs')
+  ? lib
+  : lib.default;
 
 it('should validate valid organization numbers', () => {
   const numbers = ['556016-0680', '556103-4249', '5561034249', '559244-0001'];
